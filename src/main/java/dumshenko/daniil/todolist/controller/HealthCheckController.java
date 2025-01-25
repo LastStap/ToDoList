51a7ac63b0446297ba5986b33414cd513f320fdb
Daniil Dumshenko <dumshenko2003@gmail.com>
2025-01-25 16:04:21 +0100

    update API

---
+++ src/main/java/dumshenko/daniil/todolist/controller/HealthCheckController.java
@@ -0,0 +1,17 @@
+package dumshenko.daniil.todolist.controller;
+
+import org.springframework.http.HttpStatus;
+import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+@RestController
+public class HealthCheckController {
+
+    @GetMapping("/healthCheck")
+    public ResponseEntity<String> healthCheck() {
+        return ResponseEntity
+                .status(HttpStatus.OK)
+                .body("The server is up and running");
+    }
+}
